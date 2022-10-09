21c89f9228755d91c2de2420004dc1f8b7733c68
Ankitgupta016 <ankitgupta46178@gmail.com>
2022-10-09 18:08:03 +0530

    c

---
+++ index.js
@@ -0,0 +1,18 @@
+
+ function Addrow(){
+
+    var name = document.getElementById("name").value;
+    
+   //  var email = document.getElementById("email").value;
+   //  var mobile = document.getElementById("mob").value;
+    document.writeln( "Happy Diwali:" +name + "<br>");
+  
+   
+   
+   //  document.writeln("your email :" +email + "<br>")
+   //  document.writeln("your mobile no : " +mobile );
+ }
+
+
+
+
