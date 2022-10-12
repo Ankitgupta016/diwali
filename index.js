03e95fa8e77023e42f0224401b53ef7be5f65b81
Ankitgupta016 <ankitgupta46178@gmail.com>
2022-10-12 19:49:26 +0530

    ankit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
  function showInput(){
     var name = document.getElementById("user_input").value;
-    document.write("  <h1>卐  happy dipawali :" + name +  卐 +"</h1>")
+    document.write("  <h1>卐  happy dipawali  : " + name +  "   卐" + "</h1>")
     document.write("  <h3 >May your diwali bring peace and love to your life" +" </h3>")
     document.write ('<body style=" background:url(img/p.jpg) ">')
     document.body.style.backgroundRepeat = "no-repeat";
@@ -9,7 +9,8 @@
     document.body.style.textAlign = "center";
     document.body.style.marginTop = "150px";
     document.body.style.textTransform = "uppercase";
-    document.body.style.color = "#f90303";
+    // document.body.style.color = "#f90303";
+    document.body.style.color = "gold";
     
     document.body.style.fontFamily = " fantasy";
     document.body.style.fontWeight = "600";
