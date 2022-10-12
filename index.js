7ba6e245c2a528e80fe43defd03daadd0118a0cc
Ankitgupta016 <ankitgupta46178@gmail.com>
2022-10-12 23:57:36 +0530

    c

--- index.js
+++ index.js
@@ -3,24 +3,25 @@
     var name = document.getElementById("user_input").value;
     document.write("  <h1>卐  happy dipawali  : " + name +  "   卐" + "</h1>" )
     document.write("<br>");
-    document.write("<br>");
-    document.write("<br>");
-    document.write("  <h3>May your diwali bring peace and love to your life" +" </h3>")
+    // document.write("<br>");
+    // document.write("<br>");
+    document.write("  <h2>May your diwali bring peace and love to your life" +" </h2>")
     document.write ('<body style=" background:url(img/p.jpg) ">')
+    // document.write ('<h1 style=" background:blue ">')
     // document.write('<img src="img/ed.png\">');
     // document.write('<img src=img/ap.jpg width=100% height=20% border=0>')
   
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundSize = "cover";
     document.body.style.textAlign = "center";
-    document.body.style.marginTop = "100px";
+    document.body.style.marginTop = "90px";
     document.body.style.textTransform = "uppercase";
     // document.body.style.color = "#f90303";
     document.body.style.color = "gold";
     
-    document.body.style.fontFamily = " fantasy";
+  
     document.body.style.fontWeight = "600";
-    document.body.style.fontSize = "14px";
+    // document.body.style.fontSize = "12px";
   
    
 
