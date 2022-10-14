02aaa3ce3acafc7f637994758ed6bab611f6db79
Ankitgupta016 <ankitgupta46178@gmail.com>
2022-10-15 00:01:41 +0530

    change

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 
  function showInput(){
     var name = document.getElementById("user_input").value;
-    document.write("  <h1>卐  happy dipawali  : " + name +  "   卐" + "</h1>" )
-    document.write("<br>");
-    // document.write("<br>");
-    // document.write("<br>");
-    document.write("  <h2 >May your diwali bring peace and love to your life" +" </h2>")
-    document.write ('<body style=" background:url(img/p.jpg) ">')
-    // document.write ('<h1 style=" background:blue ">')
-    // document.write('<img src="img/ed.png\">');
-    // document.write('<img src=img/ap.jpg width=100% height=20% border=0>')
-  
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.textAlign = "center";
-    document.body.style.marginTop = "90px";
-    document.body.style.textTransform = "uppercase";
-    // document.body.style.color = "#f90303";
-    document.body.style.color = "gold";
-    
-  
-    document.body.style.fontWeight = "600";
-    // document.body.style.fontSize = "12px";
-  
-   
 
-   
+    document.write( '<h1 style=" background:#b30606;width: fit-content;margin: 0 auto; " >' + " 卐  happy dipawali  : " + name + "   卐" +"</h1>" + "<br>")
+
+
+
+ document.write('<h2 style=" background:#982736;width: fit-content;margin: 0 auto; " >' + "May your diwali bring peace and love to your life "  + "</h2>")
+
+    document.write ('<body style=" background:url(img/ankit.jpg) ">' )
+
+    document.body.style.backgroundRepeat = "no-repeat";
+
+    document.body.style.backgroundSize = "cover";
+
+    document.body.style.textAlign = "center";
+
+    document.body.style.marginTop = "90px";
+
+    document.body.style.textTransform = "uppercase";
+
+    // document.body.style.color = "#f90303";
+
+    document.body.style.color = "gold";
+
+    document.body.style.width =" 100%";
+
+    document.body.style.height =" 100%";
+
+    document.body.style.fontWeight = "600";
+  
    //  document.writeln("your email :" +email + "<br>")
+
    //  document.writeln("your mobile no : " +mobile );
  }
 
