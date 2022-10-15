ffe93a084eff87391abcaff230bb253bfc473327
Ankitgupta016 <ankitgupta46178@gmail.com>
2022-10-15 23:32:00 +0530

    change

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 
- function showInput(){
-    var name = document.getElementById("user_input").value;
-    document.write('<video  style=" width: 100%;min-height: 100%;" autoplay  loop  src=\"Happy.mp4\">"+ "</video>');
+function showInput() {
+  var name = document.getElementById("user_input").value;
 
-    document.write( '<h1   style="position: absolute;width: 100%;margin: 0 auto; top: 0;margin-top: 40px; " >' + " 卐  happy dipawali  : " + name + "   卐" +"</h1>" + "<br>")
-  
-
- document.write('<h2 style="position: absolute;width: 100%;margin: 0 auto; top: 0;margin-top: 134px; " >' + "May your diwali bring peace and love to your life "  + "</h2>")
+  // document.write('<video  style=" min-height: 100%;" autoplay  loop  src=\"Happy.mp4\">"+ "</video>');
 
 
-   //  document.write ('<body style=" background:url(img/ankit.jpg) ">' )
-   //  document.write("<video src=\"Happy Diwali.mp4\">"+ "</video>");
+  // document.write( '<h1   style="position: absolute;width: 100%;margin: 0 auto; top: 0;margin-top: 40px; " >' + " 卐  happy dipawali  : " + name + "   卐" +"</h1>" + "<br>")
+  // document.write('<h2 style="position: absolute;width: 100%;margin: 0 auto; top: 0;margin-top: 134px; " >' + "May your diwali bring peace and love to your life "  + "</h2>")
 
-    document.body.style.backgroundRepeat = "no-repeat";
 
-    document.body.style.backgroundSize = "cover";
+  document.write('<h1   style="background:#3e0505;  width:fit-content;margin: 0 auto; margin-top: 25px; " >' + " 卐  happy dipawali  : " + name + "   卐" + "</h1>" + "<br>")
+  document.write('<h2 style=" background:#231919; width:fit-content; margin: 0 auto; top: 0; " >' + "May your diwali bring peace and love to your life " + "</h2>")
 
-    document.body.style.textAlign = "center";
 
-    document.body.style.margin = "0px";
-    document.body.style.padding = "0px";
+  document.write('<body style=" background:url(img/ankit.jpg) ">')
 
-    document.body.style.textTransform = "uppercase";
+  document.body.style.backgroundRepeat = "no-repeat";
 
-    // document.body.style.color = "#f90303";
+  document.body.style.backgroundSize = "cover";
 
-    document.body.style.color = "gold";
+  document.body.style.textAlign = "center";
 
-    document.body.style.width =" 100%";
+  document.body.style.margin = "0px";
+  document.body.style.padding = "0px";
 
-    document.body.style.height =" 100%";
+  document.body.style.textTransform = "uppercase";
 
-    document.body.style.fontWeight = "600";
-  
-    document.body.style.background = "black";
-  
-   //  document.writeln("your email :" +email + "<br>")
+  // document.body.style.color = "#f90303";
 
-   //  document.writeln("your mobile no : " +mobile );
- }
+  document.body.style.color = "gold";
+  document.body.style.width = "100%";
+  document.body.style.height= "100%";
+
+  document.body.style.fontWeight = "600";
+
+
+  //  document.writeln("your email :" +email + "<br>")
+
+  //  document.writeln("your mobile no : " +mobile );
+}
 
 
 
