df85a7a2ea9d7581dffd6518582bfd32a3458814
Ankitgupta016 <ankitgupta46178@gmail.com>
2022-10-18 01:22:05 +0530

    change

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
 function showInput() {
   var name = document.getElementById("user_input").value;
-// document.write('<video  style=" min-height: 100%;" autoplay  loop  src=\"\">"+ "</video>');
+document.write('<video  style=" min-height: 100%; width:100%;" autoplay  loop  src=\"v.mp4\">"+ "</video>');
 
   
 
-  // document.write( '<h1   style="position: absolute;width: 100%;margin: 0 auto; top: 0;margin-top: 40px; " >' + " 卐  happy dipawali  : " + name + "   卐" +"</h1>" + "<br>")
-  // document.write('<h2 style="position: absolute;width: 100%;margin: 0 auto; top: 0;margin-top: 134px; " >' + "May your diwali bring peace and love to your life "  + "</h2>")
+  document.write( '<h1   style="position: absolute;width: 100%;margin: 0 auto; top: 0;margin-top: 40px; " >' + " 卐  happy dipawali  : " + name + "   卐" +"</h1>" + "<br>")
+  document.write('<h2 style="position: absolute;width: 100%;margin: 0 auto; top: 0;margin-top: 134px; " >' + "May your diwali bring peace and love to your life "  + "</h2>")
 
 
-  document.write('<h1   style="background:#3e0505;  width:fit-content;margin: 0 auto; margin-top: 25px; " >' + " 卐  happy dipawali  : " + name + "   卐" + "</h1>" + "<br>")
-  document.write('<h2 style=" background:#231919; width:fit-content; margin: 0 auto; top: 0; " >' + "May your diwali bring peace and love to your life " + "</h2>")
+  // document.write('<h1   style="background:#3e0505;  width:fit-content;margin: 0 auto; margin-top: 25px; " >' + " 卐  happy dipawali  : " + name + "   卐" + "</h1>" + "<br>")
+  // document.write('<h2 style=" background:#231919; width:fit-content; margin: 0 auto; top: 0; " >' + "May your diwali bring peace and love to your life " + "</h2>")
 
 
-  document.write('<body style=" background:url(img/ankit.jpg) ">')
+  // document.write('<body style=" background:url(img/ankit.jpg) ">')
 
   document.body.style.backgroundRepeat = "no-repeat";
 
